f1bb102b72488aae1df447751d4425e965f25767
frangf212 <grandefrancisco43@gmail.com>
2024-11-28 12:17:26 +0100

    rectangulo

--- ejercicio3/src/Main.kt
+++ ejercicio3/src/Main.kt
@@ -5,4 +5,15 @@ fun main() {
     print(Math.PI * radio * radio)
     println("La circunferencia del círculo es ")
     print(2 * Math.PI * radio)
+
+
+    println("introduce un numero")
+      val a: Int = readln().toInt()
+      val b: Int = readln().toInt()
+      println("este es el perimetro")
+      var numero0 = 2*(a+b)
+      println(numero0)
+      println("este es el area")
+      var numero1 = a * b
+      println(numero1)
 }
